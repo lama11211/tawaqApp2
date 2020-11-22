cac3f817d5c5f3cae2c41d53a2aa6a0f0382290e
lama11211 <llama11211@gmail.com>
2020-11-22 21:24:15 +0300

    project

---
+++ app/src/main/java/com/example/volunteer/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.volunteer;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class volunteerWork extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void backToTrainingTracks(View V){
+
+        Intent i = new Intent(this,trainingTracks.class);
+        startActivity(i);
+        finish();
+    }
+
+    public void backToMenu(View V){
+
+        Intent i = new Intent(this,theMenu.class);
+        startActivity(i);
+        finish();
+    }
+}
